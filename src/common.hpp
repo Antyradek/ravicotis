5a03de3428e08f061c5f0cbcf882f7065d9a29cf
Antyradek <antyradek@gmail.com>
2017-02-08 23:50:06 +0100

    Change version saving type

--- src/common.hpp
+++ src/common.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
 #define NAME "Ravicotis"
-#define VERSION std::string("0.1.1")
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 1
+
+#define VERSION std::to_string(VERSION_MAJOR)).append(".").append(std::to_string(VERSION_MINOR)).append(".").append(std::to_string(VERSION_PATCH)
+
 #define WINDOW_WIDTH 2000
 #define WINDOW_HEIGHT 2000
